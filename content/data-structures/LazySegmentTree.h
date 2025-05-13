4a1e91f1e062abf974916a3e07e2878f354b0354
coin50 <chu285@purdue.edu>
2025-05-13 14:53:36 -0500

    Update LazySegmentTree.h

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,68 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Author: PURDUE
+ * Description: TAG (a,b) means apply f(x) = ax+b to all elements in a range. Segments contain either MAX or +, and you're able to recompute a segment if all elements have a TAG applied to it.
+ * Usage: On each test case, reset the value of N. Initialize seg (and len) from i = N to 2N, and iterate backwards from N-1 to 0 to build seg and lazy
  */
-#pragma once
+using T = long long;
+using TAG = pair<T,T>;
+const T defT = 0;
+const TAG defTAG = make_pair(1,0);
 
-#include "../various/BumpAllocator.h"
+int N;
+const int MAX = 200000;
+TAG lazy[MAX]; //must first build these arrays
+T seg[2*MAX];
+int len[2*MAX];
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
-};
+T f( T a, T b ){ //combine segments
+    return a+b;
+}
+void apply( TAG f, int i ){
+    TAG g = lazy[i];
+    if(i < N) lazy[i] = make_pair( f.first*g.first, f.first*g.second + f.second ); //compose two tags
+    seg[i] = f.first*seg[i] + f.second*len[i]; //evaluate tag on segment
+}
+
+void hammer_down( int i ){
+    for( int k = 20; k > 0; k-- ){
+        int j = (i>>k);
+        if(j==0) continue;
+        apply( lazy[j], 2*j );
+        apply( lazy[j], 2*j+1 );
+        lazy[j] = defTAG;
+    }
+}
+void boiler_up( int i ){
+    i /= 2;
+    while(i){
+        seg[i] = f(seg[2*i],seg[2*i+1]);
+        i /= 2;
+    }
+}
+void update( int l, int r, TAG tag ){
+    l += N, r += N;
+    int l0 = l, r0 = r;
+    while(l0%2==0) l0 /= 2;
+    while(r0%2==0) r0 /= 2;
+    hammer_down(l0);
+    hammer_down(r0-1);
+    while( l < r ){
+        if(l&1) apply(tag,l++);
+        if(r&1) apply(tag,--r);
+        l /= 2, r /= 2;
+    }
+    boiler_up(l0);
+    boiler_up(r0-1);
+}
+T query( int l, int r ){ //range sum on [l,r)
+    l += N, r += N;
+    hammer_down(l);
+    hammer_down(r-1);
+    T m1 = defT, m2 = defT;
+    while( l < r ){
+        if(l&1) m1 = f(m1,seg[l++]);
+        if(r&1) m2 = f(seg[--r],m2);
+        l /= 2, r /= 2;
+    }
+    return f(m1,m2);
+}
